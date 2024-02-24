d3478bd91a6e96217b174557f790deacec64e767
Your Name <you@example.com>
2024-02-24 11:13:52 +0530

    1st part completed - backend

---
+++ backend/db/connectToMongoDb.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectToMongoDb = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log("connected to mongodb");
+    } catch (error) {
+        console.log("Error",error.message);
+    }
+}
+
+export default connectToMongoDb;
